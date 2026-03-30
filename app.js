7092ca4db65dad004bef2c7931375f0bdb58298f
sailokeshnalabothu <sailokeshnalabothu@gmail.com>
2026-03-30 10:27:55 +0530

    updated app.js

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ app.get('/', (req, res) => {
   res.send('Hello DevOps 🚀');
 });
 
+app.get('/health', (req, res) => {
+  res.status(200).json({ status: 'ok' });
+});
+
 app.listen(3000, () => {
   console.log('Server running on port 3000');
 });
